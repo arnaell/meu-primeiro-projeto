eeef31b2a7cfbc0a856438fff592e8618c53ff8f
arnaell <arnaell@gmail.com>
2019-12-08 13:09:45 -0300

    Versão inicial do projeto.

---
+++ src/test/java/com/zoca/api/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoca.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
